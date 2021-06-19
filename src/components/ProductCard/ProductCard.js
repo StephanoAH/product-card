5020a4a48fb95d72a87dd7f7e2a910b237111cc3
StephanoAH <stephanoah@protonmail.com>
2021-06-18 23:02:55 -0400

    Product card is ready
    All the markup is done and the css need optimization
    Needed another component for the details

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Shoes from "../../assets/air-jordan-transparent.png";
 import styles from "./ProductCard.module.css";
 
 function ProductCard() {
@@ -9,9 +10,11 @@ function ProductCard() {
           <div className={styles.CircleContainer}>
             <div className={styles.Circle} />
           </div>
+          <h3 className={styles.ProductBrand}>Nike air</h3>
           <div className={styles.ProductContainer}>
-            <h3 className={styles.ProductBrand}></h3>
-            <img className={styles.ProductImg} src="" alt="" />
+            <div className={styles.ShoeContainer}>
+              <img className={styles.Shoe} src={Shoes} alt="Shoes" />
+            </div>
           </div>
         </div>
         <div className={styles.BottomContainer}>
